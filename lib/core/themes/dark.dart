1b12891667784a9abddc690a84dc8e238ebc11db
salahu01 <cvsalahu12@gmail.com>
2024-07-14 23:35:04 +0530

    added folder structure

---
+++ lib/core/themes/dark.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+final darkTheme = ThemeData.dark(useMaterial3: true).copyWith(
+  colorScheme: ColorScheme.fromSeed(
+    seedColor: Colors.deepPurple,
+  ),
+);
---
+++ lib/core/themes/light.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+final lightTheme = ThemeData.light(useMaterial3: true).copyWith(
+  colorScheme: ColorScheme.fromSeed(
+    seedColor: Colors.deepPurple,
+  ),
+);
